7ceb5d76d6d01e2c2e3116c7b9d666c3aa87babb
Jacques Germishuys <jacquesg@striata.com>
2020-04-19 13:16:49 +0100

    Initial version

---
+++ lib/Dist/Zilla/MintingProfile/Author/JACQUESG.pm
@@ -0,0 +1,38 @@
+package Dist::Zilla::MintingProfile::Author::JACQUESG;
+
+use strict;
+use warnings;
+
+use Moose;
+with 'Dist::Zilla::Role::MintingProfile::ShareDir';
+
+=head1 NAME
+
+Dist::Zilla::MintingProfile::Author::JACQUESG - Minting profile used by JACQUESG
+
+=head1 SYNOPSIS
+
+    $ dzil new -P Author::JACQUESG Some::Module
+
+=head1 DESCRIPTION
+
+B<Dist::Zilla::MintingProfile::Author::JACQUESG> is the L<Dist::Zilla> minting
+profile used by JACQUESG.
+
+=head1 AUTHOR
+
+Jacques Germishuys <jacquesg@cpan.org>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2020 Jacques Germishuys.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1; # End of Dist::Zilla::MintingProfile::Author::JACQUESG
